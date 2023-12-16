6f8d83cfb4d39c89e0cef3651edcf3866bbb9a60
Leah Anestos <leahkanestos@gmail.com>
2023-12-16 00:24:31 -0500

    footer adjusted

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../../pages/style.css';
-import { Container, Row, Col } from 'reactstrap';
+import {Col } from 'reactstrap';
 import Text from '../Homepage/Text';
 import headShot from '../../Assets/headShot.png';
+import Footer from '../Footer/Footer';
 
 
 function Home() {
